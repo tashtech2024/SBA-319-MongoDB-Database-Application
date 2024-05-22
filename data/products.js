b528f0fa7202059ee0e1cdf936b488f40e0dacff
Latasha Davis <latashaddavis21@gmail.com>
2024-05-22 18:51:24 -0400

    added html and css

---
+++ data/products.js
@@ -0,0 +1,35 @@
+const products = [
+{
+    id: 1,
+    userId: 1, 
+    title: "Product 1",
+    body: String,
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Nibh tortor id aliquet lectus."
+    }, 
+    {
+        id: 2,
+        userid: 2, 
+        title: String,
+        body: String,
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Nibh tortor id aliquet lectus."
+
+
+    },
+    {
+        id: 3,
+        userid: 3, 
+        title: String,
+        body: String,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Nibh tortor id aliquet lectus."
+    },
+    {
+        id: 4,
+        userid: 2, 
+        title: String,
+        body: String,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Nibh tortor id aliquet lectus."  
+    }
+
+
+];
+module.exports = products;
